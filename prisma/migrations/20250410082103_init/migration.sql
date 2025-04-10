0b5d1c24e172ebbf9bac57b4436cd1c710d2b905
Danny <dannysteve0693@gmail.com>
2025-04-10 19:14:29 +0700

    Added package

---
+++ prisma/migrations/20250410082103_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" SERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_slug_key" ON "Link"("slug");
